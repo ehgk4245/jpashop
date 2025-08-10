ad7948aa1977cbea067a7ffa923caa77911217e3
ehgk4245 <ehgkrk14@naver.com>
2025-08-10 13:21:21 +0900

    회원관련 기능

---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,53 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원가입")
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        assertThat(member).isEqualTo(memberRepository.findById(saveId));
+    }
+
+    @Test
+    @DisplayName("중복 회원 예외")
+    void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        Member member2 = new Member();
+        member1.setName("kim");
+        member2.setName("kim");
+
+        // when
+        memberService.join(member1);
+
+        // then
+        assertThatThrownBy(() -> memberService.join(member2))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("이미 존재하는 회원입니다.");
+    }
+}
